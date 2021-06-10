0d63074ca3d546ccd01d11fe8760fa77707da44f
seokmanHong <smhong@studiomate.kr>
2021-06-10 15:44:07 +0900

    feat: init

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         OwenIt\Auditing\AuditingServiceProvider::class,
+        Kreait\Laravel\Firebase\ServiceProvider::class,
 
         /*
          * Application Service Providers...
